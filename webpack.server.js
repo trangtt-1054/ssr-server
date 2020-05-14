7129afb0166006fe01b1acb9d4eb787e35308202
Tran Thu Trang <tranthutrang@Trans-MacBook-Pro.local>
2020-05-14 22:20:15 +0700

    continue

--- webpack.server.js
+++ webpack.server.js
@@ -1,11 +1,12 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-  target: 'node',   //tell webpack we're building a bundle for nodeJS, rather than for the browser
-  entry: './src/index.js',  //tell webpack the root file of our server application
+  mode: 'development',
+  target: 'node', //tell webpack we're building a bundle for nodeJS, rather than for the browser
+  entry: './src/index.js', //tell webpack the root file of our server application
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'build'),
   },
   //run babel on every file it runs through
   module: {
@@ -18,10 +19,10 @@ module.exports = {
           presets: [
             'react',
             'stage-0',
-            ['env', {targets: { browsers: ['last 2 versions']}}] //run all the transform rules needed to meet requirements of 2 latest versions of all popular browsers
-          ]
-        }
-      }
-    ]
-  }
-}
+            ['env', { targets: { browsers: ['last 2 versions'] } }], //run all the transform rules needed to meet requirements of 2 latest versions of all popular browsers
+          ],
+        },
+      },
+    ],
+  },
+};
