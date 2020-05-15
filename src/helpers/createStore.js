d02bba830a76ec09e1bf26b9e4e3eed900f7c5c0
tran.thu.trangb <tran.thu.trangb@b121054-mb.local>
2020-05-15 17:00:33 +0700

    continued

---
+++ src/helpers/createStore.js
@@ -0,0 +1,10 @@
+//Redux store on server side, mục đích chủ yếu của redux store ở server side là detect đc initial loading finish TRƯỚC KHI render React app (before calling renderer fn), thế nên sẽ ko wrap provider trực tiếp vào StaticBrowser, mà sẽ đưa Rrovider vào index.js trước, rồi mới pass xuống renderer
+
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from '../client/reducers';
+
+export default () => {
+  const store = createStore(reducers, {}, applyMiddleware(thunk));
+  return store;
+}
--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
-module.exports = {
+const config = {
   mode: 'development',
   target: 'node', //tell webpack we're building a bundle for nodeJS, rather than for the browser
   entry: './src/index.js', //tell webpack the root file of our server application
@@ -8,21 +11,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
-  //run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/, //do not run babel here
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions'] } }], //run all the transform rules needed to meet requirements of 2 latest versions of all popular browsers
-          ],
-        },
-      },
-    ],
-  },
+  externals: [webpackNodeExternals()] //not bundle any libraries into output bundle on server if that library exists inside the node modules folder.
 };
+
+module.exports = merge(baseConfig, config);
