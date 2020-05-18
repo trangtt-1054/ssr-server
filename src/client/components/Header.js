7bf800750d47466c9aeff75a7d151dbdf1773f0c
tran.thu.trangb <tran.thu.trangb@b121054-mb.local>
2020-05-18 10:42:41 +0700

    style done

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -8,14 +8,16 @@ const Header = ({ auth }) => {
 
   const authButton = auth ? (<a href="/api/logout">Logout</a>) : (<a href="/api/auth/google">Login</a>)
   return (
-    <div>
-      <Link to="/">Server Side Rendering</Link>
-      <div>
-        <Link to='/users'>Users</Link>
-        <Link to='/admins'>Admin</Link>
-        {authButton}
+    <nav>
+      <div className="nav-wrapper">
+        <Link to="/" className="brand-logo">Server Side Rendering</Link>
+        <ul className="right">
+          <li><Link to='/users'>Users</Link></li>
+          <li><Link to='/admins'>Admin</Link></li>
+          <li>{authButton}</li>
+        </ul>
       </div>
-    </div>
+    </nav>
   )
 }
 
